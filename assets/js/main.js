add3ba57682bce213f823a8cd1235491b68ecaf3
Rachit Tanwar <tanwarrachit6@gmail.com>
2023-08-09 10:03:01 +0530

    Respawn Reg Updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -75,55 +75,55 @@
             },
     }; // end themesflatTheme
 
-    // Start things up
-    themesflatTheme.init();
-    $(".btn-close").click(function(){
-        $("#popup_bid").modal('hide');
-    });
-    var ajaxContactForm = function () {
-        $('#contactform,#commentform').each(function () {
-            $(this).validate({
-                submitHandler: function (form) {
-                    var $form = $(form),
-                        str = $form.serialize(),
-                        loading = $('<div />', { 'class': 'loading' });
+    // // Start things up
+    // themesflatTheme.init();
+    // $(".btn-close").click(function(){
+    //     $("#popup_bid").modal('hide');
+    // });
+    // var ajaxContactForm = function () {
+    //     $('#contactform,#commentform').each(function () {
+    //         $(this).validate({
+    //             submitHandler: function (form) {
+    //                 var $form = $(form),
+    //                     str = $form.serialize(),
+    //                     loading = $('<div />', { 'class': 'loading' });
 
-                    $.ajax({
-                        type: "POST",
-                        url: $form.attr('action'),
-                        data: str,
-                        beforeSend: function () {
-                            $form.find('.form-submit,comment-form').append(loading);
-                        },
-                        success: function (msg) {
-                            var result, cls;
-                            if (msg === 'Success') {
-                                result = 'Message Sent Successfully To Email Administrator. ( You can change the email management a very easy way to get the message of customers in the user manual )';
-                                cls = 'msg-success';
-                            } else {
-                                result = 'Error sending email.';
-                                cls = 'msg-error';
-                            }
+    //                 $.ajax({
+    //                     type: "POST",
+    //                     url: $form.attr('action'),
+    //                     data: str,
+    //                     beforeSend: function () {
+    //                         $form.find('.form-submit,comment-form').append(loading);
+    //                     },
+    //                     success: function (msg) {
+    //                         var result, cls;
+    //                         if (msg === 'Success') {
+    //                             result = 'Message Sent Successfully To Email Administrator. ( You can change the email management a very easy way to get the message of customers in the user manual )';
+    //                             cls = 'msg-success';
+    //                         } else {
+    //                             result = 'Error sending email.';
+    //                             cls = 'msg-error';
+    //                         }
 
-                            $form.prepend(
-                                $('<div />', {
-                                    'class': 'flat-alert ' + cls,
-                                    'text': result
-                                }).append(
-                                    $('<a class="close" href="#"><i class="fa fa-close"></i></a>')
-                                )
-                            );
+    //                         $form.prepend(
+    //                             $('<div />', {
+    //                                 'class': 'flat-alert ' + cls,
+    //                                 'text': result
+    //                             }).append(
+    //                                 $('<a class="close" href="#"><i class="fa fa-close"></i></a>')
+    //                             )
+    //                         );
 
-                            $form.find(':input').not('.submit').val('');
-                        },
-                        complete: function (xhr, status, error_thrown) {
-                            $form.find('.loading').remove();
-                        }
-                    });
-                }
-            });
-        }); // each contactform
-    };
+    //                         $form.find(':input').not('.submit').val('');
+    //                     },
+    //                     complete: function (xhr, status, error_thrown) {
+    //                         $form.find('.loading').remove();
+    //                     }
+    //                 });
+    //             }
+    //         });
+    //     }); // each contactform
+    // };
 
     // Dark Light Mode
     if($('.body').hasClass('is_dark')) {
@@ -642,3 +642,35 @@ window.requestAnimFrame = function () {
     setInterval(loop, 1000 / 60);
   };
   
+
+  function addPlayer() {
+    const playersContainer = document.getElementById("players-container");
+    const playerNumber = playersContainer.children.length + 1;
+  
+    const playerDiv = document.createElement("div");
+    playerDiv.className = "player";
+  
+    const playerName = document.createElement("input");
+    playerName.type = "text";
+    playerName.placeholder = "Player " + playerNumber + " Name";
+    playerName.name = "player_name_" + playerNumber;
+  
+    const isCaptainLabel = document.createElement("label");
+    isCaptainLabel.innerHTML = "Captain:";
+    
+    const isCaptain = document.createElement("input");
+    isCaptain.type = "radio";
+    isCaptain.name = "captain";
+    isCaptain.value = playerNumber;
+  
+    playerDiv.appendChild(playerName);
+    playerDiv.appendChild(isCaptainLabel);
+    playerDiv.appendChild(isCaptain);
+  
+    playersContainer.appendChild(playerDiv);
+  }
+  
+  // function submitForm() {
+  //   // Collect and process form data here
+  //   alert("Form submitted!");
+  // }
